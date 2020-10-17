bfdb79688e07ecf752c64a2b5fe8103a3529e255
Carlos Vigueras <carlosj.vigueras@gmail.com>
2020-10-17 16:55:55 +0200

    Docs agregados

---
+++ docs/precache-manifest.49af57b65b6dc3b4462c15ab2576c3b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15525aa7e66983f960d87d64ea21bd2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ecfcb40442940f0e2db",
+    "url": "/static/css/main.c5018a6d.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ecfcb40442940f0e2db",
+    "url": "/static/js/main.145ad91d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
